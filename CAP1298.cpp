a587aaa04cf7809c826deee22fee4638de787093
Nicolas Biard <nico645@outlook.com>
2024-10-09 19:01:09 -0400

    Update CAP1298.cpp

--- CAP1298.cpp
+++ CAP1298.cpp
@@ -2,7 +2,7 @@
 
 CAP1298::CAP1298(gpio_num_t sda, gpio_num_t scl, uint32_t freq, uint8_t address)
 {
-    this->busController = new I2CController(address, sda, scl, freq);
+    this->busController = I2CController::getInstance();
 }
 
 CAP1298::~CAP1298()
@@ -17,11 +17,11 @@ esp_err_t CAP1298::begin()
     if (ret == ESP_OK)
     {
         tx_buffer[0] = 0x8C;
-        ret |= this->busController->writeByte(tx_buffer, CAP1298_MULTIPLE_TOUCH_CONFIG);
+        ret |= this->busController->writeByte(CAP1298_I2C_ADDRESS, tx_buffer, CAP1298_MULTIPLE_TOUCH_CONFIG);
         tx_buffer[0] = 0x5F;
-        ret |= this->busController->writeByte(tx_buffer, CAP1298_SENSITIVITY_CONTROL);
+        ret |= this->busController->writeByte(CAP1298_I2C_ADDRESS, tx_buffer, CAP1298_SENSITIVITY_CONTROL);
         tx_buffer[0] = 0x00;
-        ret |= this->busController->writeByte(tx_buffer, CAP1298_REPEAT_RATE_ENABLE);
+        ret |= this->busController->writeByte(CAP1298_I2C_ADDRESS, tx_buffer, CAP1298_REPEAT_RATE_ENABLE);
     }
     return ret;
 }
@@ -30,11 +30,11 @@ esp_err_t CAP1298::setupInterrupt(uint8_t channel)
 {
     esp_err_t ret;
     uint8_t tx_buffer[1];
-    this->busController->readByte(tx_buffer, CAP1298_INTERRUPT_ENABLE);
+    this->busController->readByte(CAP1298_I2C_ADDRESS, tx_buffer, CAP1298_INTERRUPT_ENABLE);
 
     tx_buffer[0] |= (1 << channel);
 
-    ret = this->busController->writeByte(tx_buffer, CAP1298_INTERRUPT_ENABLE);
+    ret = this->busController->writeByte(CAP1298_I2C_ADDRESS, tx_buffer, CAP1298_INTERRUPT_ENABLE);
     
     return ret;
 }
@@ -43,19 +43,19 @@ bool CAP1298::touchStatusChanged()
 {
     uint8_t rx_buffer[1];
     uint8_t tx_buffer[1];
-    this->busController->readByte(rx_buffer, CAP1298_MAIN_CONTROL);
+    this->busController->readByte(CAP1298_I2C_ADDRESS, rx_buffer, CAP1298_MAIN_CONTROL);
     bool touchStatus = rx_buffer[0] & CAP1298_INT_MASK;
     
     // reset the interrupt
     tx_buffer[0] = 0x00;
-    this->busController->writeByte(tx_buffer, CAP1298_MAIN_CONTROL);
+    this->busController->writeByte(CAP1298_I2C_ADDRESS, tx_buffer, CAP1298_MAIN_CONTROL);
     return touchStatus;
 }
 
 void CAP1298::updateTouchStatus()
 {
     uint8_t rx_buffer[1];
-    this->busController->readByte(rx_buffer, CAP1298_INPUT_STATUS);
+    this->busController->readByte(CAP1298_I2C_ADDRESS, rx_buffer, CAP1298_INPUT_STATUS);
 
     m_newTouches = (m_touchData ^ rx_buffer[0]) & rx_buffer[0];
 	m_newReleases = (m_touchData ^ rx_buffer[0]) & m_touchData;
